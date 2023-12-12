c5a75a241e78804ba89270c3cda4fe2dab313d1a
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-12-11 19:53:06 -0500

    photo7

--- src/pages/router/Photos.jsx
+++ src/pages/router/Photos.jsx
@@ -1,17 +1,59 @@
+import { useContext, useState } from "react";
 import Image from "next/image";
 import modelos from "../modelos/Modelos";
+import { useMenuContext } from "../../context/MenuContext";
 import styles from "../../styles/index.module.scss";
 
 const Photos = () => {
+
+  const { menuOpen } = useMenuContext();
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % modelos.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + modelos.length) % modelos.length
+    );
+  };
+
+  const translateValue = -currentIndex * 100;
+
   return (
     <>
-      <section className={styles.photos}>
-        {modelos.map((url, index) => (
-          <picture key={index}>
-            <Image src={url} alt={`Image ${index}`} width={350} height={400} />
-          </picture>
-        ))}
+      <main className={`${styles.Photos} ${menuOpen ? styles.layoutOpen : ""}`}>
+      <section className={styles.slider}>
+        <button className={styles.prevBtn} onClick={prevSlide}>
+          &lt;
+        </button>
+        <div
+          className={styles.slideWrapper}
+          style={{ transform: `translateX(${translateValue}%)` }}
+        >
+          {modelos.map((url, index) => (
+            <picture
+              key={index}
+              className={`${styles.slide} ${
+                index === currentIndex ? styles.active : ""
+              }`}
+            >
+              <Image
+                src={url}
+                alt={`Slide ${index + 1}`}
+                width={50}
+                height={50}
+              />
+            </picture>
+          ))}
+        </div>
+        <button className={styles.nextBtn} onClick={nextSlide}>
+          &gt;
+        </button>
       </section>
+      </main>
     </>
   );
 };
