2e943f96bea2918c0730b91eb6079a47b6c27a97
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-12-12 09:53:22 -0500

    photo10

--- src/pages/router/Photos.jsx
+++ src/pages/router/Photos.jsx
@@ -1,59 +1,46 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards } from "swiper/modules";
 import Image from "next/image";
 import modelos from "../modelos/Modelos";
 import { useMenuContext } from "../../context/MenuContext";
 import styles from "../../styles/index.module.scss";
+import "swiper/css";
+import "swiper/css/effect-cards";
 
 const Photos = () => {
-
   const { menuOpen } = useMenuContext();
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % modelos.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + modelos.length) % modelos.length
-    );
-  };
-
-  const translateValue = -currentIndex * 100;
 
   return (
     <>
-      <main className={`${styles.Photos} ${menuOpen ? styles.PhotosOpacity : ""}`}>
-      <section className={styles.slider}>
-        <button className={styles.prevBtn} onClick={prevSlide}>
-          &lt;
-        </button>
-        <div
-          className={styles.slideWrapper}
-          style={{ transform: `translateX(${translateValue}%)` }}
-        >
-          {modelos.map((url, index) => (
-            <picture
-              key={index}
-              className={`${styles.slide} ${
-                index === currentIndex ? styles.active : ""
-              }`}
-            >
-              <Image
-                src={url}
-                alt={`Slide ${index + 1}`}
-                width={50}
-                height={50}
-              />
-            </picture>
-          ))}
-        </div>
-        <button className={styles.nextBtn} onClick={nextSlide}>
-          &gt;
-        </button>
-      </section>
+      <section className={`${styles.Photos} ${menuOpen ? styles.PhotosOpacity : ""}`}>
+      <h1 className={styles.tittle}>Photos Briefcase</h1>
+      <main>
+        <section>
+          <Swiper
+            effect={"cards"}
+            grabCursor={true}
+            modules={[EffectCards]}
+            className="mySwiper"
+            spaceBetween={15} // Espacio entre las tarjetas
+            slidesPerView={1} // Número de tarjetas visibles
+          >
+            {modelos.map((url, index) => (
+              <SwiperSlide key={index}>
+                <picture>
+                  <Image
+                    src={url}
+                    alt={`Slide ${index + 1}`}
+                    width={250}
+                    height={350}
+                  />
+                </picture>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </section>
       </main>
+      </section>
     </>
   );
 };
