154cf0de851e6291cbbe32fc1f0f6b17dcb9cf5b
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-12-12 17:20:30 -0500

    photo12

---
+++ src/pages/modelos/videos.js
@@ -0,0 +1,32 @@
+const videos = [
+    {
+        url: "https://www.youtube.com/embed/ZCdWpndXhLg",
+        tittle: "Goze Vídeo"
+    },
+    {
+        url: "https://www.youtube.com/embed/ZCdWpndXhLg",
+        tittle: "EXPO Panamá"
+    },
+    {
+        url: "https://www.youtube.com/embed/ZCdWpndXhLg",
+        tittle: "Teaser boda el valle"
+    },
+    {
+        url: "https://www.youtube.com/embed/ZCdWpndXhLg",
+        tittle: "Nike La City"
+    },
+    {
+        url: "https://www.youtube.com/embed/ZCdWpndXhLg",
+        tittle: "Nesh Del Monte - Vencer"
+    },
+    {
+        url: "https://www.youtube.com/embed/ZCdWpndXhLg",
+        tittle: "Cristopher Bustamante Away Without Leave"
+    },
+    {
+        url: "https://www.youtube.com/embed/ZCdWpndXhLg",
+        tittle: "Póstumo - pfnme correcion portada"
+    }
+]
+
+export default videos;
--- src/pages/router/Films.jsx
+++ src/pages/router/Films.jsx
@@ -1,5 +1,47 @@
-const Films = () => {
-    return 
-}
+import { useContext } from "react";
+import { useMenuContext } from "@/context/MenuContext";
+import Image from "next/image";
+import videos from "../modelos/videos";
+import styles from "../../styles/index.module.scss";
 
-export default Films
+const Films = () => {
+  const { menuOpen } = useMenuContext();
+
+  return (
+    <>
+      <section
+        className={`${styles.Videos} ${menuOpen ? styles.VideosOpacity : ""}`}
+      >
+        <main>
+          <h1>All My Films</h1>
+
+          <picture>
+            <Image
+              src="https://static.wixstatic.com/media/fac730_eef0f62c152c419f81608ac0ce550909~mv2.jpg/v1/fill/w_763,h_343,al_b,q_80,enc_auto/fac730_eef0f62c152c419f81608ac0ce550909~mv2.jpg"
+              alt="img"
+              width={150}
+              height={100}
+            />
+          </picture>
+
+          <section>
+            {videos.map((url, index) => (
+              <div key={index}>
+                <iframe
+                  src={url.url}
+                  title="YouTube Video Player"
+                  frameBorder="0"
+                  allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+                <h2>{url.tittle}</h2>
+              </div>
+            ))}
+          </section>
+        </main>
+      </section>
+    </>
+  );
+};
+
+export default Films;
