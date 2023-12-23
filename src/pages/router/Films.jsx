ed54785ab7afbcddd7d4e8d60ffc4ebe57c79434
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-12-23 09:36:33 -0500

    photo19

--- src/pages/router/Films.jsx
+++ src/pages/router/Films.jsx
@@ -28,7 +28,7 @@ const Films = () => {
           initial={{  opacity: 0 }}
           animate={{opacity: 1 }}
           exit={{  opacity: 0 }}
-          transition={{ duration: 0.5, delay: 0.7 }}
+          transition={{ duration: 0.5, delay: 1 }}
         >
           <main>
             <h1>All My Films</h1>
